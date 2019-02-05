385ab7e101c4c69765beaced7b01a30bcb4c24cc
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-02-05 19:32:09 +0000

    just commit as is - working still

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module OmniauthLab
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.force_ssl = true
+    #config.force_ssl = true
+    OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
   end
 end
